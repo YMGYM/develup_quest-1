5ef2ed19548daf53cf2ca022f3a992ce58779102
/Users/leny/Leny/잡동사니/미연시\ 프로젝트/minjun/game/gui.rpy /Users/leny/Leny/잡동사니/미연시\ 프로젝트/minjun/game/options.rpy /Users/leny/Leny/잡동사니/미연시\ 프로젝트/minjun/game/screens.rpy /Users/leny/Ldrone8449 <abthkim@naver.com>
2019-10-27 11:07:48 +0900

    try1_to_fix_hoverproblem

--- findpet/static/findpet/script/script.js
+++ findpet/static/findpet/script/script.js
@@ -1,13 +1,5 @@
 $(document).ready(function () {
-  $("#content div").hover(
-    function() {
-      $(this).addClass("hover");
-    }, function() {
-      $(this).removeClass("hover");
-    }
-  );
-
-  $(".hover").each(function () {
+  $("#{{ pet.id }}").each(function () {
     $(this).qtip({
       content: {
         text: '<img src=" {{ l.thumb }} " />'
